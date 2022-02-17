9c787bf893c2ca5a2ff60cb18a9eb5d07fcc21e4
araujobruno0 <96297989+araujobruno0@users.noreply.github.com>
2022-02-17 17:03:26 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+import Tela1 from './telas/Tela1';
+import Tela2 from './telas/Tela2';
+
+const Stack = createStackNavigator();
+
+export default function App(){
+   return(
+     <NavigationContainer>
+<Stack.Navigator>
+  
+  <Stack.Screen name = "Tela1" component = {Tela1} options = {{headerShown: false}}/>
+  <Stack.Screen name = "Tela2" component = {Tela2} options = {{headerShown: false}}/>
+
+</Stack.Navigator>
+
+     </NavigationContainer>
+
+   );
+
+
+
+
+}
